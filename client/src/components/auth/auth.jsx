452484f508d8158cee0f5edec1775986758aeb2a
yurimovo <yuri22091983@gmail.com>
2022-03-08 22:52:26 +0600

    start migrating to MUI

--- client/src/components/auth/auth.jsx
+++ client/src/components/auth/auth.jsx
@@ -1,64 +1,147 @@
-import React, { useState } from 'react'
-import { Button } from "react-bootstrap"
-import { authorization } from "../../actions/user"
-import { useDispatch } from "react-redux"
-import { login } from "../../store/actions/authActions"
+import React, {useEffect, useState} from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import {useNavigate} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Container from "@material-ui/core/Container";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './auth.css'
-import {useNavigate} from "react-router-dom"
-import Input from "../../ui/text-fields/input-register/input-register"
+const useStyles = makeStyles((theme) => ({
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        height: '83vh',
+        alignContent: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'grey'
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    }
+}))
 
 export const Auth = () => {
-    const [userName, setUserName] = useState('')
-    const [password, setPassword] = useState('')
-    const history = useNavigate()
-    const dispatch = useDispatch()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailDirty, setEmailDirty] = useState(false);
+    const [passwordDirty, setPasswordDirty] = useState(false);
+    const [emailError, setEmailError] = useState('Email must not be empty');
+    const [passwordError, setPasswordError] = useState('Password must not be empty');
+    const [formValid, setFormValid] = useState(false);
+    const history = useNavigate();
+    const classes = useStyles();
 
-    const handleLogin = async () => {
-        await authorization(userName, password)
-        dispatch(login())
-        /*setUserName('')
-        setPassword('')*/
-        history('/')
+    const emailHandler = (e) => {
+        setEmail(e.target.value);
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Incorrect Email');
+        } else {
+            setEmailError('');
+        }
+    };
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value);
+         if (e.target.value.length < 4 || e.target.value.length > 8) {
+           setPasswordError('Password length must be longer than 4 and shorter than 8 symbols')
+           if (!e.target.value) {
+             setPasswordError('Password must not be empty')
+           }
+         } else {
+           setPassword('')
+         }
+    };
+
+    const blurHandler = (e) => {
+        if (e.target.name === 'email') {
+            setEmailDirty(true);
+        } else if (e.target.name === 'password') {
+            setPasswordDirty(true);
+        }
     }
 
+    useEffect(() => {
+        if (emailError || passwordError) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    }, [emailError, passwordError])
+
+    /*const handleLogin = async () => {
+        try {
+            await doRegister(email, password);
+            history.push('/');
+        } catch (e) {
+            console.log(e.message);
+        }
+    }*/
+
     return (
-        <div className={'container'} style={{
-            height: '83vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-        }}>
-            <div className={'row'}>
-                <div className={'col-md-12'}>
-                    <div className={'main-style-auth-form text-center'}>
-                        <h4 className={'h4-auth-form'}>Авторизация</h4>
-                        <form>
-                            <Input
-                                className={'input-reg-form'}
-                                value={userName}
-                                setValue={setUserName}
-                                type="text"
-                                placeholder={'Имя пользователя'}
-                            />
-                            <Input
-                                className={'input-reg-form'}
-                                value={password}
-                                setValue={setPassword}
-                                type="password"
-                                placeholder={'Пароль'}
-                            />
-                            <Button
-                                className={'btn btn-submit btn-style-auth-form'}
-                                onClick={handleLogin}
-                            >
-                                ОК
-                            </Button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Container component={'main'} maxWidth={'xs'}>
+            <form className={classes.form} noValidate>
+                <Grid container className={classes.container}>
+                    <Grid item sm={12}>
+                        <Typography variant={'h5'} className={classes.title}>
+                            Авторизация
+                        </Typography>
+                    </Grid>
+                    <Grid item sm={4}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email"
+                            name="email"
+                            autoComplete="email"
+                            type={'email'}
+                            placeholder={'mail_name@example.com'}
+                            onBlur={e => blurHandler(e)}
+                            value={email}
+                            onChange={e => emailHandler(e)}
+                        />
+                    </Grid>
+                    <Grid item sm={4}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={password}
+                            onChange={e => passwordHandler(e)}
+                        />
+                    </Grid>
+                    <Grid item sm={4}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            /*onClick={handleLogin}*/
+                            disabled={!formValid}
+                        >
+                            Войти
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="/register" variant="body2">
+                                    Нет учетной записи? Зарегистрируйтесь
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </form>
+        </Container>
     )
 }
--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,22 +1,147 @@
-import React from 'react'
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core';
+import {Link, useNavigate} from 'react-router-dom';
 
-import SlikiLogo from '../../media/logo.png'
+import HeaderBackground from '../../media/bg-header.png';
+import HeaderLogo from '../../media/logo.png';
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../../store/actions/authActions";
 
-import './header.css'
-import AuthButton from "../auth-button/auth-button"
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1
+    },
+    appBar: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        backgroundImage: `url(${HeaderBackground})`
+    },
+    menuButton: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'right',
+      marginRight: theme.spacing(1)
+    },
+    headerButton: {
+        marginLeft: theme.spacing(1)
+    },
+    gridContainer: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        marginLeft: theme.spacing(8)
+    },
+    gridItemLogo: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    headerLink: {
+        textDecoration: 'none',
+        color: 'aliceblue',
+        '&:hover': {
+            textDecoration: 'none',
+            color: 'aliceblue'
+        },
+        '&:visited': {
+            textDecoration: 'none',
+            color: 'aliceblue'
+        }
+    }
+}));
 
 const Header = () => {
+
+    const classes = useStyles();
+    const auth = useSelector(state => state.auth);
+    const isAdmin = localStorage.getItem('isAdmin');
+    const history = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout())
+        history('/auth')
+    }
+
     return (
-        <div className={'container header'}>
-            <div className={'row align-items-center content'}>
-                <div className={'col-md-6'}>
-                    <img className={'rounded mx-left d-block img css-adaptive'} src={SlikiLogo} alt={'logo'}/>
-                </div>
-                <div className={'col-md-6'}>
-                    <AuthButton/>
-                </div>
-            </div>
-        </div>
+        <AppBar position={'fixed'} className={classes.appBar}>
+            <Grid container className={classes.gridContainer}>
+               <Grid item md={4} className={classes.gridItemLogo}>
+                   <img src={HeaderLogo} />
+               </Grid>
+               <Grid item md={6}>
+                   <Toolbar>
+                       {!auth ?
+                       <>
+                           <Button
+                               className={classes.headerButton}
+                               color={'primary'}
+                               variant={'contained'}
+                           >
+                               <Link className={classes.headerLink} to={'/auth'}>Войти</Link>
+                           </Button>
+                           <Button
+                               className={classes.headerButton}
+                               color={'secondary'}
+                               variant={'contained'}
+                           >
+                               <Link className={classes.headerLink} to={'/register'}>Регистрация</Link>
+                           </Button>
+                       </>
+                           :
+                       <>
+                           <Button
+                               className={classes.headerButton}
+                               color={'secondary'}
+                               variant={'contained'}
+                           >
+                               <Link
+                                   className={'nav-item'} to={'/auth'}
+                                   onClick={handleLogout}
+                               >
+                                   Выйти
+                               </Link>
+                           </Button>
+                           <Button
+                               className={classes.headerButton}
+                               color={'secondary'}
+                               variant={'contained'}
+                           >
+                               <Link className={classes.headerLink} to="/personal-page">Личный кабинет</Link>
+                           </Button>
+                       </>}
+                       {isAdmin ?
+                           <>
+                               <Button
+                                   className={classes.headerButton}
+                                   color={'secondary'}
+                                   variant={'contained'}
+                               >
+                                   <Link className={classes.headerLink} to="/race-result">Админка</Link>
+                               </Button>
+                           </> : null
+                       }
+                   </Toolbar>
+               </Grid>
+               <Grid item md={2}>
+                   <IconButton
+                       size="large"
+                       edge="start"
+                       color="inherit"
+                       aria-label="menu"
+                       sx={{ mr: 2 }}
+                       className={classes.menuButton}
+                   >
+                       <MenuIcon />
+                   </IconButton>
+               </Grid>
+            </Grid>
+        </AppBar>
     )
 }
 
